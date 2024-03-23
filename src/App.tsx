bc85fc2adc8bd8f33b7a69929328e810b144e673
Maksim Suldin <drneiron@gmail.com>
2024-03-23 16:03:31 +1100

    add maps and dotenv

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {APIProvider, Map } from '@vis.gl/react-google-maps';
+import {Polygon} from './components';
 
 function App() {
+  const position = {lat: -37.869300842285156, lng: 144.7637176513672};
+  const googleMapsApiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string;
+  // const paths = [{lat: -37.81621, lng: 144.96405}];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <APIProvider apiKey={googleMapsApiKey}>
+            <Map
+            style={{width: '100vw', height: '100vh'}}
+            defaultCenter={position}
+            defaultZoom={13}
+            gestureHandling={'greedy'}
+            disableDefaultUI={true}
+            />
+            {/* <Polygon strokeWeight={1.5} paths={paths}/> */}
+          </APIProvider>
       </header>
     </div>
   );
